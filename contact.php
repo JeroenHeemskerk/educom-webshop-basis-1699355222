b869416bd90ea529bfb3b6ccbc874385608fa6fd
Nicole Goris <nicolegoris@hotmail.nl>
2023-11-09 15:29:28 +0100

    #13 Engelstalig houden

--- contact.php
+++ contact.php
@@ -20,34 +20,34 @@
  ?>
         <form action="contact.php" method="post">
             <div class="invoervelden">
-                <label for="aanhef">Aanhef:</label>
-                    <select class="sel" id="aanhef" name="aanhef">
-                        <option value="dhr">Dhr.</option>
-                        <option value="mvr">Mvr.</option>
-                        <option value="anders">Anders</option>
+                <label for="salutation">Aanhef:</label>
+                    <select class="sel" id="salutation" name="salutation">
+                        <option value="man">Dhr.</option>
+                        <option value="woman">Mvr.</option>
+                        <option value="different">Anders</option>
                     </select><br>
                 <label for="fname">Naam:</label>
                     <input class="sw" type="text" id="fname" name="firstname" placeholder="Typ hier uw naam" value="<?php echo $name;?>"><br>
                 <label for="email">E-mailadres:</label>
                     <input class="sw" type="text" id="email" name="emailadress" placeholder="Typ hier uw e-mailadres"><br> 
-                <label for="tel">Telefoonnummer:</label>
-                    <input class="sw" type="text" id="tel" name="telnumber" placeholder="Typ hier uw telefoonnummer"><br>
+                <label for="phone">Telefoonnummer:</label>
+                    <input class="sw" type="text" id="phone" name="phonenumber" placeholder="Typ hier uw telefoonnummer"><br>
                 <label for="street">Straatnaam</label>
                     <input class="sw" type="text" id="street" name="streetname" placeholder="Typ hier uw straat"><br>  
                 <label for="strnr">Huisnummer</label>
                     <input class="sw" type="text" id="strnr" name="streetnumber" placeholder="Typ hier uw huiwsnummer"><br>
                 <label for="zpcd">Postcode</label>
                     <input class="sw" type="text" id="zpcd" name="zipcode" placeholder="Typ hier uw postcode als 1234 AB"><br>
-                <label for="wp">Woonplaats</label>
-                    <input class="sw" type="text" id="wp" name="woonplaats" placeholder="Typ hier uw woonplaats"><br>
+                <label for="resid">Woonplaats</label>
+                    <input class="sw" type="text" id="resid" name="residence" placeholder="Typ hier uw woonplaats"><br>
                 <br>   
             </div>
             <div>
             Kies uw communicatievoorkeur:<br>
                 <input type="radio" id="email" name="communicatievoorkeur" value="E-mail">
                     <label for="email">E-mail</label><br>
-                <input type="radio" id="tel" name="communicatievoorkeur" value="Telefoon">
-                    <label for="tel">Telefoon</label><br>
+                <input type="radio" id="phone" name="communicatievoorkeur" value="Phone">
+                    <label for="phone">Telefoon</label><br>
                 <input type="radio" id="pst" name="communicatievoorkeur" value="Post">
                     <label for="pst">Post</label><br>
                 <br>
